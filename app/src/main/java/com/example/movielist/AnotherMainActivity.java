cd191e209f9cba78bc749a53a7de24c3a391d390
Tarr Márton <tarr.marton1@gmail.com>
2021-11-21 22:03:40 +0100

    Biometric authentication

---
+++ app/src/main/java/com/example/movielist/AnotherMainActivity.java
@@ -0,0 +1,82 @@
+package com.example.movielist;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.example.movielist.databinding.ActivityMainBinding;
+
+public class AnotherMainActivity extends Activity {
+
+    private ActivityMainBinding binding;
+    String[] items = new String[]{"1", "2", "3", "4"};
+    //List<String> items;
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Thread(()-> {
+            MovieDatabase db = Room.databaseBuilder(getApplicationContext(),
+                    MovieDatabase.class, "movie-db").allowMainThreadQueries().build();
+
+            runOnUiThread(() ->{
+                items = new String[db.movieDAO().getAllTitles().size()];
+                db.movieDAO().getAllTitles().toArray(items);
+                binding.moviesListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+                binding.moviesListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(AnotherMainActivity.this, items[i], Toast.LENGTH_SHORT).show();
+                        MovieDetailsActivity movieDetailsActivity = new MovieDetailsActivity();
+                        movieDetailsActivity.setTitle(items[i]);
+                        Intent intent = new Intent(AnotherMainActivity.this, MovieDetailsActivity.class);
+                        intent.putExtra("title", items[i]);
+                        startActivityForResult(intent, 1);
+                        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                    }
+                });
+            });
+        }).start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //setUpDatabase
+
+
+        binding.floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AnotherMainActivity.this, PopActivity.class);
+                //startActivity(new Intent(MainActivity.this, PopActivity.class));
+                startActivityForResult(intent, 1);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Intent refresh = new Intent(this, AnotherMainActivity.class);
+            startActivity(refresh);
+            this.finish();
+        }
+    }
+}
--- app/src/main/java/com/example/movielist/MainActivity.java
+++ app/src/main/java/com/example/movielist/MainActivity.java
@@ -1,81 +1,113 @@
 package com.example.movielist;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
 
+import android.app.Activity;
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.biometric.BiometricPrompt;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.core.content.ContextCompat;
+import androidx.room.Room;
 
 import com.example.movielist.databinding.ActivityMainBinding;
 
+import java.util.concurrent.Executor;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 101010;
+    //UI
 
-    private ActivityMainBinding binding;
-    String[] items = new String[]{"1", "2", "3", "4"};
-    //List<String> items;
+    private ImageView fingerPrint;
 
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        new Thread(()-> {
-            MovieDatabase db = Room.databaseBuilder(getApplicationContext(),
-                    MovieDatabase.class, "movie-db").allowMainThreadQueries().build();
-
-            runOnUiThread(() ->{
-                items = new String[db.movieDAO().getAllTitles().size()];
-                db.movieDAO().getAllTitles().toArray(items);
-                binding.moviesListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
-                binding.moviesListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        Toast.makeText(MainActivity.this, items[i], Toast.LENGTH_SHORT).show();
-                        MovieDetailsActivity movieDetailsActivity = new MovieDetailsActivity();
-                        movieDetailsActivity.setTitle(items[i]);
-                        Intent intent = new Intent(MainActivity.this, MovieDetailsActivity.class);
-                        intent.putExtra("title", items[i]);
-                        startActivityForResult(intent, 1);
-                        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-                    }
-                });
-            });
-        }).start();
-    }
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.biometric);
 
-        //setUpDatabase
+        fingerPrint = findViewById(R.id.fingerPrint);
 
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e("MY_APP_TAG", "No biometric features available on this device.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                // Prompts the user to create credentials that your app accepts.
+                final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+                enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
+                startActivityForResult(enrollIntent, REQUEST_CODE);
+                break;
+        }
 
-        binding.floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, PopActivity.class);
-                //startActivity(new Intent(MainActivity.this, PopActivity.class));
-                startActivityForResult(intent, 1);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, AnotherMainActivity.class));
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
             }
         });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK){
-            Intent refresh = new Intent(this, MainActivity.class);
-            startActivity(refresh);
-            this.finish();
-        }
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Authenticate")
+                .setSubtitle("Log in using your fingerprint")
+                .setNegativeButtonText("Use account password")
+                .build();
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+
+        fingerPrint.setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
+        });
     }
 }
